82319675b388e44cd35f039a45ef43c71e246a2a
reaper <saip589@gmail.com>
2023-03-02 08:32:28 +0530

    update plugins

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -9,3 +9,12 @@ vim.cmd[[
     let g:gruvbox_material_menu_selection_background = 'blue'
     colorscheme gruvbox-material
 ]]
+
+-- vim.g.gruvbox_italic_functions = true
+-- vim.g.gruvbox_sidebars = { "qf", "vista_kind", "terminal", "packer" }
+-- vim.g.gruvbox_colors = { hint = "orange", error = "orange" }
+-- vim.g.gruvbox_theme = { TabLineSel = { bg = "orange" } }
+-- vim.g.gruvbox_transparent = true
+-- vim.g.gruvbox_italic_keywords = false
+-- -- Load the colorscheme
+-- vim.cmd[[colorscheme gruvbox-flat]]
