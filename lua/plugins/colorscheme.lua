5fdfb4721d42497e949ad664332efad8e6433629
reaper <saip589@gmail.com>
2024-09-05 21:32:25 +0530

    change colorscheme to gruvbox

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,54 +1,26 @@
-require("tokyonight").setup({
-  style = "night", --storm, moon, night
-  light_style = "day",
-  transparent = false,
-  terminal_colors = true,
-  styles = {
-    comments = { italic = true },
-    keywords = { italic = true },
-    functions = { bold = true },
-    variables = {},
-    sidebars = "dark",
-    floats = "dark",
+-- Default options:
+require("gruvbox").setup({
+  terminal_colors = true, -- add neovim terminal colors
+  undercurl = true,
+  underline = true,
+  bold = true,
+  italic = {
+    strings = true,
+    emphasis = true,
+    comments = true,
+    operators = false,
+    folds = true,
   },
-  sidebars = { "qf", "help" },
-  day_brightness = 0.3,
-  hide_inactive_statusline = false,
+  strikethrough = true,
+  invert_selection = false,
+  invert_signs = false,
+  invert_tabline = false,
+  invert_intend_guides = false,
+  inverse = true, -- invert background for search, diffs, statuslines and errors
+  contrast = "", -- can be "hard", "soft" or empty string
+  palette_overrides = {},
+  overrides = {},
   dim_inactive = false,
-  lualine_bold = false,
-
-  on_colors = function(colors) end,
-
-  on_highlights = function(hl, c)
-    local prompt = "#2d3149"
-    hl.TelescopeNormal = {
-      bg = c.bg_dark,
-      fg = c.fg_dark,
-    }
-    hl.TelescopeBorder = {
-      bg = c.bg_dark,
-      fg = c.bg_dark,
-    }
-    hl.TelescopePromptNormal = {
-      bg = prompt,
-    }
-    hl.TelescopePromptBorder = {
-      bg = prompt,
-      fg = prompt,
-    }
-    hl.TelescopePromptTitle = {
-      bg = c.cyan,
-      fg = c.bg_dark,
-    }
-    hl.TelescopePreviewTitle = {
-      bg = c.bg_dark,
-      fg = c.bg_dark,
-    }
-    hl.TelescopeResultsTitle = {
-      bg = c.bg_dark,
-      fg = c.bg_dark,
-    }
-  end,
+  transparent_mode = false,
 })
-
-vim.cmd("colorscheme tokyonight")
+vim.cmd("colorscheme gruvbox")
--- lua/reaper/plugins.lua
+++ lua/reaper/plugins.lua
@@ -1,6 +1,6 @@
 return {
   {
-    'folke/tokyonight.nvim',
+    "ellisonleao/gruvbox.nvim",
     lazy = false,
     priority = 1000,
     config = function()
