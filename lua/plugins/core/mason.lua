ce1ebd054d19ecdae88998bd1c76f9322a20d06d
reaper <saip589@gmail.com>
2024-12-22 15:30:15 +0530

    add emmet-ls, css-ls and tailwind-css-ls

--- lua/plugins/core/mason.lua
+++ lua/plugins/core/mason.lua
@@ -1,11 +1,11 @@
 require("mason").setup()
 require("mason-lspconfig").setup({
    ensure_installed = { 
-     -- "emmet_ls", 
-     -- "html",
-     -- "cssls",
+     "emmet_ls", 
+     "html",
+     "cssls",
      "ts_ls",
-     -- "tailwindcss",
+     "tailwindcss",
      "jsonls",
      "gopls",
      "rust_analyzer",
