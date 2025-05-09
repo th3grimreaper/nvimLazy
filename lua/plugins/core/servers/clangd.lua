af2050b1ab70eabc38b8709df9db9a9cb257838b
reaper <saip589@gmail.com>
2025-05-09 10:36:54 +0530

    perform minor fixes

--- lua/plugins/core/servers/clangd.lua
+++ lua/plugins/core/servers/clangd.lua
@@ -1,17 +1,17 @@
 local root_pattern = require("lspconfig.util").root_pattern
 return {
-  on_attach = on_attach,
-  capabilities = capabilities,
-  cmd = { "clangd" },
-  filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
-  root_dir =  root_pattern{
-      '.clangd',
-      '.clang-tidy',
-      '.clang-format',
-      'compile_commands.json',
-      'compile_flags.txt',
-      'configure.ac',
-      '.git'
-  },
-  single_file_support = true,
+	on_attach = on_attach,
+	capabilities = capabilities,
+	cmd = { "clangd" },
+	filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
+	root_dir = root_pattern({
+		".clangd",
+		".clang-tidy",
+		".clang-format",
+		"compile_commands.json",
+		"compile_flags.txt",
+		"configure.ac",
+		".git",
+	}),
+	single_file_support = true,
 }
