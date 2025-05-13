04d24daaa66f99a2a0ee9c7f3ec4ef1aa4cdf2d2
reaper <saip589@gmail.com>
2025-05-13 10:37:08 +0530

    feat: add lazy stats in dashboard

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -57,3 +57,20 @@ dashboard.section.buttons.val = {
 dashboard.opts.opts.noautocmd = true
 --vim.cmd[[autocmd User AlphaReady echo 'ready']]
 alpha.setup(dashboard.opts)
+
+vim.api.nvim_create_autocmd("User", {
+	once = true,
+	pattern = "LazyVimStarted",
+	callback = function()
+		local stats = require("lazy").stats()
+		local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+		dashboard.section.footer.val = "⚡ Neovim loaded "
+			.. stats.loaded
+			.. "/"
+			.. stats.count
+			.. " plugins in "
+			.. ms
+			.. "ms"
+		pcall(vim.cmd.AlphaRedraw)
+	end,
+})
