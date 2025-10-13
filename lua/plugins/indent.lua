a0edc9d63b5dac034ba6ffc4f8c758a1861a145e
reaper <saip589@gmail.com>
2025-10-13 12:19:55 +0530

    update mason and mason-lspconfig

--- lua/plugins/indent.lua
+++ lua/plugins/indent.lua
@@ -1,10 +1,10 @@
 vim.opt.list = true
-vim.opt.listchars:append "space:⋅"
-vim.opt.listchars:append "eol:↴"
+vim.opt.listchars:append("space:⋅")
+vim.opt.listchars:append("eol:↴")
 
-require("ibl").setup {
-  indent = {
-    char = "▎",
-    tab_char = "▎", -- This was the fix
-  },
-}
+require("ibl").setup({
+	indent = {
+		char = "▎",
+		tab_char = "▎", -- This was the fix
+	},
+})
--- lua/reaper/plugins.lua
+++ lua/reaper/plugins.lua
@@ -611,14 +611,14 @@ return {
 		-- event = "VeryLazy",
 		cmd = "Mason",
 		event = "BufReadPre",
-		commit = "fc98833b6da5de5a9c5b1446ac541577059555be",
+		-- commit = "fc98833b6da5de5a9c5b1446ac541577059555be",
 		config = function()
 			require("plugins.core.mason")
 		end,
 	},
 	{
 		"williamboman/mason-lspconfig.nvim",
-		commit = "1a31f824b9cd5bc6f342fc29e9a53b60d74af245",
+		-- commit = "1a31f824b9cd5bc6f342fc29e9a53b60d74af245",
 	},
 	{
 		"neovim/nvim-lspconfig",
