86bb24fc5fe26cbba134068f9e63c48e44d2f8b8
reaper <saip589@gmail.com>
2025-11-05 19:58:35 +0530

    modify config to improve startup time

--- lua/reaper/plugins.lua
+++ lua/reaper/plugins.lua
@@ -618,6 +618,7 @@ return {
 	},
 	{
 		"williamboman/mason-lspconfig.nvim",
+		event = "BufReadPre",
 		-- commit = "1a31f824b9cd5bc6f342fc29e9a53b60d74af245",
 		dependencies = { "neovim/nvim-lspconfig" },
 	},
