dcc185110168d001076fa000dcbc697715f0c370
reaper <saip589@gmail.com>
2025-11-05 15:34:57 +0530

    modify plugins.lua

--- lua/reaper/plugins.lua
+++ lua/reaper/plugins.lua
@@ -619,6 +619,7 @@ return {
 	{
 		"williamboman/mason-lspconfig.nvim",
 		-- commit = "1a31f824b9cd5bc6f342fc29e9a53b60d74af245",
+		dependencies = { "neovim/nvim-lspconfig" },
 	},
 	{
 		"neovim/nvim-lspconfig",
